241fc6b1214e330fd4479ff9249d0a68546f927f
ShawnSamson77 <120073580+ShawnSamson77@users.noreply.github.com>
2023-04-17 18:59:47 +0900

    Add files via upload

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -7,56 +7,64 @@
 
 int main() {
   auto start_time = std::chrono::high_resolution_clock::now();
-
-  int n = 1e+9;
-  int range = 100;
+  int n = 50;
+  int range = 200;
   std::vector<int> key(n);
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
-    //printf("%d ",key[i]);
+    printf("%d ",key[i]);
   }
-  //printf("\n");
+  printf("\n");
 
   std::vector<int> bucket(range,0); 
   auto end_time = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> elapsed_time = end_time - start_time;
   std::cout << "Single thread calculate time: " << elapsed_time.count() << " seconds" << std::endl;
-
   auto openMP_start_time = std::chrono::high_resolution_clock::now();
+
 //#pragma omp parallel for
   for (int i=0; i<n; i++)
-//#pragma omp atomic update
+//#pragma omp atomic update    //Single thread faster than omp atomic update
     bucket[key[i]]++;
 
   std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
+  std::vector<int> temporal(range,0);
+  
+#pragma omp parallel for
+  for (int i=0; i<range; i++)
+    offset[i] = bucket[i];
+  for(int j=1; j<range; j<<=1){
+#pragma omp parallel for
+    for(int i=0; i<range;i++){
+      temporal[i] = offset[i];
+    }
+#pragma omp parallel for
+    for(int i=j; i<range;i++){
+      offset[i] += temporal[i-j];
+    }
+  }
 
 #pragma omp parallel for
   for (int i=0; i<range; i++) {
-    int j = offset[i];
+    int j;
+    if (i > 0){j = offset[i-1];}
+    else{j = 0;}
     for (; bucket[i]>0; bucket[i]--) {
       key[j++] = i;
     }
   }
   end_time = std::chrono::high_resolution_clock::now();
 
-  //for (int i=0; i<n; i++) {printf("%d ",key[i]);}
-  //printf("\n");
+  for (int i=0; i<n; i++) {printf("%d ",key[i]);}
+  printf("\n");
 
   elapsed_time = end_time - openMP_start_time;
   std::chrono::duration<double> All_time = end_time - start_time;
   std::cout << "openMP calculate time: " << elapsed_time.count() << " seconds" << std::endl;
   std::cout << "Total calculate time: " << All_time.count() << " seconds" << std::endl;
-
 }
-
-//Single thread part, n = 1e+9, 10.8 sec;  n = 2e+9, 22.3 sec
-//n = 1e+9,  1 thread   11.8  sec
-//n = 1e+9, 24 threads  11.5  sec
-
-//n = 2e+9,  1 thread   24.2  sec
-//n = 2e+9, 24 threads  23.5  sec
-
-
-
+//My laptop i7-1185G7 @3.00GHz, DDR4 16GB 4267MHz
+//Single thread part, n = 1e+8, range = 1e+8, 0.91 sec
+//n = 1e+8,  1 thread   5.39  sec
+//n = 1e+8,  8 threads  4.01  sec
+//g++ -fopenmp -O3 14_bucket_sort.cpp
