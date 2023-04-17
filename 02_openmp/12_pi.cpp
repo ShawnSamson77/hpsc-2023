96260ee67c7831a4273eb56886824b0edb8cba7b
ShawnSamson77 <120073580+ShawnSamson77@users.noreply.github.com>
2023-04-17 18:57:51 +0900

    Add files via upload

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -1,25 +1,29 @@
 #include <cstdio>
 #include <omp.h>
-#include <time.h>
+#include <chrono>
+#include <iostream>
 
 int main() {
+  auto start_time = std::chrono::high_resolution_clock::now();
   int n = 2e+9;
   double dx = 1. / n;
   double pi = 0;
-  time_t start_time, end_time;
 
-  start_time = time(NULL);
 #pragma omp parallel for reduction(+:pi)
   for (int i=0; i<n; i++) {
     double x = (i + 0.5) * dx;
     pi += 4.0 / (1.0 + x * x) * dx;
   }
-  end_time = time(NULL);
-
+  
   printf("%17.15f\n",pi);
-  printf("calculation time: %ld sec\n",end_time - start_time);
+  auto end_time = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed_time = end_time - start_time;
+  std::cout << "calculate time: " << elapsed_time.count() << " seconds" << std::endl;
 }
 
 //My laptop i7-1185G7 @3.00GHz, DDR4 16GB 4267MHz
-//n = 2e+9, 1 thread  11 sec
-//n = 2e+9, 8 threads  2 sec
+//n = 2e+9, 1 thread  2.04 sec
+//n = 2e+9, 8 threads 0.56 sec
+
+//g++ 12_pi.cpp -fopenmp -O3
+
