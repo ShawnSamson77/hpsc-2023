fba736c16fcd4ea52ed091675a98f6c7f9fb6260
ShawnSamson77 <120073580+ShawnSamson77@users.noreply.github.com>
2023-04-22 16:12:02 +0900

    Add files via upload

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -1,26 +1,49 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+#include <immintrin.h>
+
+float reduce(int N, __m256 avec){
+  float a[N];
+  __m256 bvec = _mm256_permute2f128_ps(avec,avec,1);
+  bvec = _mm256_add_ps(bvec,avec);
+  bvec = _mm256_hadd_ps(bvec,bvec);
+  bvec = _mm256_hadd_ps(bvec,bvec);
+  _mm256_store_ps(a, bvec);
+  return a[0];
+}
 
 int main() {
   const int N = 8;
-  float x[N], y[N], m[N], fx[N], fy[N];
+  float x[N], y[N], m[N], fx[N], fy[N], j[N];  //add j[N]
   for(int i=0; i<N; i++) {
     x[i] = drand48();
     y[i] = drand48();
     m[i] = drand48();
     fx[i] = fy[i] = 0;
+    j[i] = i;
   }
+
+  __m256 xvec = _mm256_load_ps(x);
+  __m256 yvec = _mm256_load_ps(y);
+  __m256 mvec = _mm256_load_ps(m);
+  __m256 jvec = _mm256_load_ps(j);
+  __m256 mask, rxvec, ryvec, rvec, m3rvec, fxvec, fyvec;
+
   for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      if(i != j) {
-        float rx = x[i] - x[j];
-        float ry = y[i] - y[j];
-        float r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
-      }
-    }
+    mask = _mm256_cmp_ps(_mm256_set1_ps(i), jvec, _CMP_EQ_OQ);  //if i == j output=1 else output=0
+
+    rxvec = _mm256_sub_ps(_mm256_set1_ps(x[i]), xvec);  //rx = x[i] -x [j]
+    ryvec = _mm256_sub_ps(_mm256_set1_ps(y[i]), yvec);  //ry = y[i] - y[j]
+    rvec = _mm256_sqrt_ps(_mm256_add_ps(_mm256_mul_ps(rxvec, rxvec), _mm256_mul_ps(ryvec, ryvec)));  //sqrt(rx*rx+ry*ry)
+
+    m3rvec = _mm256_div_ps(mvec, _mm256_mul_ps(_mm256_mul_ps(rvec, rvec), rvec));  //  m / (r*r*r)
+    fxvec = _mm256_blendv_ps(_mm256_mul_ps(m3rvec, rxvec), _mm256_set1_ps(0), mask);  //if mask==1 -> 0, mask==0 -> calculate
+    fyvec = _mm256_blendv_ps(_mm256_mul_ps(m3rvec, ryvec), _mm256_set1_ps(0), mask);
+
+    fx[i] -= reduce(N, fxvec);
+    fy[i] -= reduce(N, fyvec);
+      
     printf("%d %g %g\n",i,fx[i],fy[i]);
   }
 }
